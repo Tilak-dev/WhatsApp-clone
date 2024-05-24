717b8a2a57bb59f12ca426ea10b2f953379caa47
Tilak Singh <tsingh99991@gmail.com>
2024-05-25 00:26:36 +0530

    works almost done in top chat bar info

--- src/components/right-body/RightBodyContainer.jsx
+++ src/components/right-body/RightBodyContainer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import TypeMsg from "./TypeMsg";
+import TopChatInfo from "./TopChatInfo";
 
 function RightBodyContainer() {
   return (
@@ -7,6 +8,7 @@ function RightBodyContainer() {
       className="w-[708px] h-[585.33px] bg-[#111b21]
     border-l-[0.6px] border-[#282d30] relative"
     >
+      <TopChatInfo />
       <TypeMsg />
     </div>
   );
---
+++ src/components/right-body/TopChatInfo.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { PiVideoCameraFill } from "react-icons/pi";
+import { FaAngleDown } from "react-icons/fa6";
+import { IoMdSearch } from "react-icons/io";
+import { LuMoreVertical } from "react-icons/lu";
+
+
+function TopChatInfo() {
+  return (
+    <>
+      <header
+        className=" w-[708px] h-[59px] bg-[#202c33] pt-2.5 pb-2.5 pl-4 pr-4 flex flex-row items-center
+    "
+      >
+        <div className="pr-[15px] mt-[-1px]">
+          <img src="./dp-img.png" alt="dp_amg" className=" w-10 h-10" />
+        </div>
+        <div className="w-[434px] h-[21px] text-[#e9edef]">
+          <span className=" text-base">Kunal</span>
+        </div>
+        <div className="w-[167px] h-[42px] text-[#8696a0] justify-end text-2xl flex flex-row items-center gap-2">
+          <div className="flex flex-row">
+            <div className="flex flex-row text-[#4f6979] items-center ml-2.5 pt-[5px] pr-2.5 pl-2.5 pb-[3px]">
+              <PiVideoCameraFill className=" pr-1.5" />
+              <FaAngleDown className=" text-[13px]" />
+            </div>
+          </div>
+          <div className=" p-2">
+            <IoMdSearch />
+          </div>
+          <div className=" p-2">
+            <LuMoreVertical />
+          </div>
+        </div>
+      </header>
+    </>
+  );
+}
+
+export default TopChatInfo;
